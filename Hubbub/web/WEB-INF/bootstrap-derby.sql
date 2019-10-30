31ddfb4f6201a3d58a0416b9892d4248b7860e27
Sean Gilkey <sgilkey@austincc.edu>
2019-10-30 11:10:12 -0500

    Finished out bootstrap-derby.sql with some initial post data.

--- Hubbub/web/WEB-INF/bootstrap-derby.sql
+++ Hubbub/web/WEB-INF/bootstrap-derby.sql
@@ -1,3 +1,4 @@
+DROP TABLE posts;
 DROP TABLE users;
 
 CREATE TABLE users (
@@ -13,9 +14,14 @@ CREATE TABLE posts (
     posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
     CONSTRAINT pk_posts PRIMARY KEY (id),
-    CONSTRAINT fk_post_author FOREIGN KEY author REFERENCES users(username)
+    CONSTRAINT fk_post_author FOREIGN KEY (author) REFERENCES users(username)
 );
 
 INSERT INTO users (username, passhash) VALUES
     ('johndoe', '$5$JI3G06zK$S.ZZCjQFhTYEdPfcGenqkK.s8nIr0KW4jXsqDEwBCX8'),
     ('janedoe', '$5$JI3G06zK$S.ZZCjQFhTYEdPfcGenqkK.s8nIr0KW4jXsqDEwBCX8');
+
+INSERT INTO posts (author, posted, content) VALUES
+    ('johndoe', '2017-05-09 08:23:47.110', 'My first Hubbub post! #JavaRules #J2EERocks'),
+    ('johndoe', '2017-06-02 19:00:05.965', 'I''ve invited Jane to join.'),
+    ('janedoe', '2018-01-13 06:30:45.888', 'Alright, I''ve signed up. Now what?');
